31fa7bc39254adccdbe4454442898ec2ec5b4958
jie.bao@emc.com <jie.bao@emc.com>
2016-05-23 11:48:42 +0800

    Add a base class for import CSV file

--- assets/admin.py
+++ assets/admin.py
@@ -7,35 +7,45 @@ from .actions import import_csv, export_csv
 from .models import IPAddress, Server, Project, User
 
 
-class IPAddressAdmin(admin.ModelAdmin):
-    list_display = ('ip_address', 'owner', 'project', 'hostname', 'status', )
-    search_fields = ['ip_address', 'owner__id', 'project__name', 'status']
-    csv_fields = ('ip_address', 'owner', 'project', 'hostname', 'status', 'mac_address')
-
+class ModelAdminWithImportView(admin.ModelAdmin):
     def get_urls(self):
-        urls = super(IPAddressAdmin, self).get_urls()
+        urls = super(ModelAdminWithImportView, self).get_urls()
+        
+        opts = self.model._meta
         my_urls = [
             url(r'^import/$', self.admin_site.admin_view(self.import_csv),
-                name="assets_ipaddress_import"),
+                name="assets_"+opts.model_name+"_import"),
         ]
         return my_urls + urls
-
+    
     def import_csv(self, request):
         return import_csv(self, request)
-            
 
-class ServerAdmin(admin.ModelAdmin):
+
+class IPAddressAdmin(ModelAdminWithImportView):
+    list_display = ('ip_address', 'owner', 'project', 'hostname', 'status', )
+    search_fields = ['ip_address', 'owner__id', 'project__name', 'status']
+    csv_fields = ('ip_address', 'owner', 'project',
+                  'hostname', 'status', 'mac_address')
+
+
+class ServerAdmin(ModelAdminWithImportView):
     list_display = ('server_id', 'primary_ip', 'project', 'state')
+    csv_fields = ('server_id', 'primary_ip', 'controll_ip', 'first_nic_mac',
+                  'interface_port', 'vlan_mode', 'operation_system', 
+                  'default_user', 'default_password', 'state', 'comments')
 
 
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(ModelAdminWithImportView):
     list_display = ('name', 'description', 'owner', 'created_at')
     fields = ('name', 'description', 'owner', 'created_at')
+    csv_fields = ('name', 'description', 'owner', 'created_at',
+                  'terminated_at', 'deleted')
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ModelAdminWithImportView):
     list_display = ('id', 'name', 'email')
-    
+    csv_fields = ('id', 'name', 'email', 'resign')
 
 
 admin.site.register(IPAddress, IPAddressAdmin)
